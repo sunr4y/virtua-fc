84f7e13c133c3caba912c3f2f07083a0bc74e14c
Pablo Román <pablo.roman85@gmail.com>
2026-03-27 19:43:12 +0100

    Remove beta feedback request cronjob and related code (#492)

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('horizon:snapshot')->everyFiveMinutes();
 Schedule::command('app:cleanup-games')->dailyAt('04:00');
-Schedule::command('app:send-beta-feedback-requests')->hourly();
-Schedule::command('beta:send-invite-reminders')->dailyAt('10:00');
 // Schedule::command('beta:invite-waitlist '.config('beta.daily_invites'))->dailyAt('21:00');
