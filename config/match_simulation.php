759fb8648dbaf3c41333899090e8ebc1e82e3314
Pablo Román <pablo.roman85@gmail.com>
2026-04-04 17:04:46 +0200

    Tweak dixon-coles rho

--- config/match_simulation.php
+++ config/match_simulation.php
@@ -105,7 +105,7 @@ return [
     |   is sampled from the probability distribution.
     |
     */
-    'dixon_coles_rho' => -0.13,         // goal correlation: 0 = independent Poisson, -0.13 = realistic
+    'dixon_coles_rho' => -0.05,         // goal correlation: 0 = independent Poisson, -0.13 = realistic
     'max_goals_cap' => 6,
     'score_concentration' => 1.5,       // 1.0 = standard, >1 = results cluster closer to xG mode
 
